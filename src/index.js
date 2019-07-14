c88b4d151f6fddcb9eb68054909adc6148b6bbd8
Artur Rowinski <arowinski96@gmail.com>
2019-07-14 18:37:38 +0200

    Add absolute imports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import Root from "./views/Root/Root";
+import "index.css";
+import Root from "views/Root/Root";
 
 ReactDOM.render(<Root />, document.getElementById("root"));
