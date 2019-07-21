0ab3f54feb403d8ffde9480b044012b0b045792a
Artur Rowinski <arowinski96@gmail.com>
2019-07-21 08:31:36 +0200

    Add Logo as link

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,10 +2,12 @@ import React from "react";
 import Button from "../Button/Button";
 import styles from "./Navbar.module.scss";
 import { Link } from "react-router-dom";
+import Logo from "../Logo/Logo";
 
 const Navbar = ({ openModalFn }) => (
   <nav className={styles.wrapper}>
     <div className={styles.left} />
+    <Logo />
     <div className={styles.right}>
       <Link to="/login">Zaloguj</Link>
       <Button second onClick={openModalFn}>
