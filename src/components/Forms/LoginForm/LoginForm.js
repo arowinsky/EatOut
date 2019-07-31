4b1982323b5a43a9648bc83c637f3a67063cc0ac
Artur Rowinski <arowinski96@gmail.com>
2019-07-31 21:42:44 +0200

    Change LoginForm component from function component to class commponent

--- src/components/Forms/LoginForm/LoginForm.js
+++ src/components/Forms/LoginForm/LoginForm.js
@@ -4,27 +4,31 @@ import Button from "../../Button/Button";
 import Input from "../../Input/Input";
 import Title from "../../Title/Title";
 
-const LoginForm = ({ submitFn }) => (
-  <div className={styles.wrapper}>
-    <Title>Logowanie:</Title>
-    <form autoComplete="off" className={styles.form} onSubmit={submitFn}>
-      <Input
-        type="text"
-        name="email"
-        label="Podaj adres e-mail"
-        maxLength={20}
-      />
-      <Input
-        type="password"
-        name="password1"
-        label="Podaj hasło"
-        maxLength={20}
-      />
-      <Button second type="submit">
-        Zaloguj
-      </Button>
-    </form>
-  </div>
-);
+class LoginForm extends React.Component {
+  render() {
+    return (
+      <div className={styles.wrapper}>
+        <Title>Logowanie:</Title>
+        <form autoComplete="off" className={styles.form}>
+          <Input
+            type="text"
+            name="email"
+            label="Podaj adres e-mail"
+            maxLength={20}
+          />
+          <Input
+            type="password"
+            name="password1"
+            label="Podaj hasło"
+            maxLength={20}
+          />
+          <Button second type="submit">
+            Zaloguj
+          </Button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default LoginForm;
