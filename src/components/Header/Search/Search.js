a13a461c71cf1387a9f7494b8ec99ca5a09a21de
Artur Rowinski <arowinski96@gmail.com>
2019-08-11 12:44:27 +0200

    Final corrections

--- src/components/Header/Search/Search.js
+++ src/components/Header/Search/Search.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import styles from './Search.module.scss';
-import ReactDOM from 'react-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearchLocation } from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import styles from "./Search.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearchLocation } from "@fortawesome/free-solid-svg-icons";
 
-
-const Search = () =>(
-    <form>
-        <div className={styles.search_container}>
-            <input className={styles.search_input} type="text"/>
-            <button type="submit" className={styles.search_icon}> <FontAwesomeIcon icon={faSearchLocation} /></button>
-        </div>
-    </form>
-)
+const Search = () => (
+  <form>
+    <div className={styles.search_container}>
+      <input className={styles.search_input} type="text" />
+      <button type="submit" className={styles.search_icon}>
+        {" "}
+        <FontAwesomeIcon icon={faSearchLocation} />
+      </button>
+    </div>
+  </form>
+);
 
 export default Search;
