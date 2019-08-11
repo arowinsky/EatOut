2a6b73674e4570b3aee950b3cefdb0febca6f14f
Katie Otto <33523737+kateotto@users.noreply.github.com>
2019-08-11 12:06:58 +0200

    set navbar styling

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 import styles from "./Logo.modules.scss";
 
 const Logo = () => (
-  <Link to="/" className={styles.link}>
-    EatOut
-  </Link>
+    <Link to="/" className={styles.link} style={{ textDecoration: 'none', color: 'white', letterSpacing: '1.5px' }}>
+      <div className={styles.logo_element}>EatOut</div>
+    </Link>
 );
 
 export default Logo;
