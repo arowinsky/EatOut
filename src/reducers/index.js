95868f860d3a1c620c7cc9bce01ceedc672cdc19
Artur Rowinski <arowinski96@gmail.com>
2019-08-13 21:59:53 +0200

    Changes: -install redux & react-redux -create simple store & reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+const initialState = {
+  users: []
+};
+
+const rootReducer = (state = initialState, action) => {
+  console.log(action);
+};
+
+export default rootReducer;
---
+++ src/store/index.js
@@ -0,0 +1,6 @@
+import { createStore } from "redux";
+import eatoutApp from "../reducers";
+
+const store = createStore(eatoutApp);
+
+export default store;
