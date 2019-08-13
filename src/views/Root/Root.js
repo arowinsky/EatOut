7646371b0ceaa40489f5c718312420d8b1c7273f
Artur Rowinski <arowinski96@gmail.com>
2019-08-13 22:22:33 +0200

    Changes: -import Provider to our Root component -import store to Root component -wrap our app in the store

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./index.scss";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "store";
 import HomeView from "../HomeView/HomeView";
 import LoginView from "../LoginView/LoginView";
 import RegisterView from "../RegisterView/RegisterView";
@@ -10,18 +12,20 @@ import Stage from "../../components/Header/Stage/Stage";
 class Root extends React.Component {
   render() {
     return (
-      <BrowserRouter>
-        <>
-          <Header />
-          <Stage/>
-          <h1>Działa!</h1>
-          <Switch>
-            <Route exact path="/" component={HomeView} />
-            <Route path="/login" component={LoginView} />
-            <Route path="/register" component={RegisterView} />
-          </Switch>
-        </>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <>
+            <Header />
+            <Stage />
+            <h1>Działa!</h1>
+            <Switch>
+              <Route exact path="/" component={HomeView} />
+              <Route path="/login" component={LoginView} />
+              <Route path="/register" component={RegisterView} />
+            </Switch>
+          </>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
