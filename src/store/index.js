87e39f897b6a1e3fb0ca82e7a1d749a51a6f8d2b
Artur Rowinski <arowinski96@gmail.com>
2019-08-14 10:18:17 +0200

    Add option to view our store in Redux devTools

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,9 @@
 import { createStore } from "redux";
 import eatoutApp from "../reducers";
 
-const store = createStore(eatoutApp);
+const store = createStore(
+  eatoutApp /* preloadedState, */,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store;
