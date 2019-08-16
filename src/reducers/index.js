e0d36e7c20179fe3bf8897a12ef5a63ad40c247f
Artur Rowinski <arowinski96@gmail.com>
2019-08-16 16:24:10 +0200

    Delete not needed

--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-const initialState = {
-  users: []
-};
-
-const rootReducer = (state = initialState, action) => {
-  console.log(action);
-};
-
-export default rootReducer;
--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,8 +1,6 @@
 import React from "react";
 import "./index.scss";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "store";
 import HomeView from "../HomeView/HomeView";
 import LoginView from "../LoginView/LoginView";
 import RegisterView from "../RegisterView/RegisterView";
@@ -12,20 +10,18 @@ import Stage from "../../components/Header/Stage/Stage";
 class Root extends React.Component {
   render() {
     return (
-      <Provider store={store}>
-        <BrowserRouter>
-          <>
-            <Header />
-            <Stage />
-            <h1>Działa!</h1>
-            <Switch>
-              <Route exact path="/" component={HomeView} />
-              <Route path="/login" component={LoginView} />
-              <Route path="/register" component={RegisterView} />
-            </Switch>
-          </>
-        </BrowserRouter>
-      </Provider>
+      <BrowserRouter>
+        <>
+          <Header />
+          <Stage />
+          <h1>Działa!</h1>
+          <Switch>
+            <Route exact path="/" component={HomeView} />
+            <Route path="/login" component={LoginView} />
+            <Route path="/register" component={RegisterView} />
+          </Switch>
+        </>
+      </BrowserRouter>
     );
   }
 }
