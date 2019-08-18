f4aca611f3107f45ed6adea7b257d83c7d2292e5
Artur Rowinski <arowinski96@gmail.com>
2019-08-18 16:31:10 +0200

    Little clear code

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -4,19 +4,16 @@ const initState = {
 const authReducer = (state = initState, action) => {
   switch (action.type) {
     case "LOGIN_ERROR":
-      console.log("Błąd logowania");
       return {
         ...state,
         authError: "Nie udało się zalogować"
       };
     case "LOGIN_SUCCESS":
-      console.log("Logowanie udane");
       return {
         ...state,
         authError: null
       };
     case "SIGNOUT_SUCCESS":
-      console.log("uytkownik został wylogowany");
       return state;
     default:
       return state;
