8afe540d47a934586206ff5bbc400a4d4839550d
Katie Otto <33523737+kateotto@users.noreply.github.com>
2019-09-08 19:20:08 +0200

    add firt part of new local form

--- src/components/LocalOwner/OwnerBox/OwnerBox.js
+++ src/components/LocalOwner/OwnerBox/OwnerBox.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import styles from './OwnerBox.module.scss';
+import Button from '../../Button/Button';
+import {Link} from 'react-router-dom';
 
 class OwnerBox extends React.Component {
     constructor(props) {
@@ -12,7 +14,11 @@ class OwnerBox extends React.Component {
             ?
                  <div className={styles.box_wrapper}>Twoja restauracja to:</div> 
             : 
-                <div className={styles.box_wrapper}>Nie masz jeszcze restauracji</div>))
+                <div className={styles.box_wrapper}>
+                    Nie masz jeszcze restauracji
+                    <br/>
+                <Link to="/add-new-local-1"><Button second >Dodaj swój lokal</Button></Link>
+                </div>))
     }
 }
 
