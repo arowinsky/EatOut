51fd257e26a86a8dd3aa477239d0c671b86a70a6
Artur Rowinski <arowinski96@gmail.com>
2019-11-15 22:04:12 +0100

    Refactor appearance EatingPlaceProfileCard

--- src/components/LocalOwner/RestaurantProfile/EatingPlaceProfileCard/EatingPlaceProfileCard.js
+++ src/components/LocalOwner/RestaurantProfile/EatingPlaceProfileCard/EatingPlaceProfileCard.js
@@ -5,7 +5,11 @@ const EatingPlaceProfileCard = ({ restaurantAvatar }) => {
   console.log(restaurantAvatar);
   return (
     <div className={styles.card}>
-      <img src={restaurantAvatar} alt="eatingPlaceAvatar" />
+      <img
+        className={styles.restaurantAvatar}
+        src={restaurantAvatar}
+        alt="eatingPlaceAvatar"
+      />
       <div className={styles.container}>
         <p>Poczekalnia</p>
         <p>Mickiewicza 38, Poznań</p>
