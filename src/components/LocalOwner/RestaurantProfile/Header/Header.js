cf066043fd9bef4962c96a7d7b452e41d75d7e7f
Artur Rowinski <arowinski96@gmail.com>
2019-11-16 21:24:00 +0100

    Get & set data in RestaurantProfile/Header

--- src/components/LocalOwner/RestaurantProfile/Header/Header.js
+++ src/components/LocalOwner/RestaurantProfile/Header/Header.js
@@ -1,19 +1,33 @@
-import React from 'react';
-import styles from './Header.module.scss';
+import React from "react";
+import styles from "./Header.module.scss";
+import { connect } from "react-redux";
 
 class RestaurantHeader extends React.Component {
-    render() {
-        return (
-            <div>
-               <div className={styles.headerWrapper}></div>
-               <div className={styles.iconWrapper}></div>
-               <div className={styles.adressWrapper}>
-                   <div className={styles.title}>Nazwa restauracji</div>
-                   <div>ul. Przykładowa 12, Poznań</div>
-                   <div>dzielnica</div>
-               </div>
-            </div>
-        )
+  render() {
+    const { haveEatingPlace } = this.props;
+    let restaurantName;
+    let restaurantStreet;
+    if (haveEatingPlace) {
+      restaurantName = haveEatingPlace.restaurantName;
+      restaurantStreet = haveEatingPlace.restaurantStreet;
     }
+    return (
+      <div>
+        <div className={styles.headerWrapper}></div>
+        <div className={styles.iconWrapper}></div>
+        <div className={styles.adressWrapper}>
+          <div className={styles.title}>{restaurantName}</div>
+          <div>{restaurantStreet}</div>
+          <div>dzielnica</div>
+        </div>
+      </div>
+    );
+  }
 }
-export default RestaurantHeader;
+const mapStateToProps = state => {
+  return {
+    haveEatingPlace: state.auth.haveEatingPlace
+  };
+};
+
+export default connect(mapStateToProps)(RestaurantHeader);
