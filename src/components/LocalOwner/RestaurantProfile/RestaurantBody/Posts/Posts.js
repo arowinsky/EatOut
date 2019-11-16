33d7c983b53ba54d11eb2a7f29d3ee62171b83b7
Artur Rowinski <arowinski96@gmail.com>
2019-11-17 00:10:56 +0100

    Add condition  that only show template to add new post if currently user is owner this eatingplace

--- src/components/LocalOwner/RestaurantProfile/RestaurantBody/Posts/Posts.js
+++ src/components/LocalOwner/RestaurantProfile/RestaurantBody/Posts/Posts.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import styles from './Posts.module.scss';
-import PostItem from './PostItem/PostItem';
-import PostForm from './PostForm/PostForm';
-
+import React from "react";
+import styles from "./Posts.module.scss";
+import PostItem from "./PostItem/PostItem";
+import PostForm from "./PostForm/PostForm";
+import { connect } from "react-redux";
 
 class Posts extends React.Component {
-    render(){
-        return (
-            <div>
-                <PostItem/>
-                <PostForm/>
-            </div>
-        )
+  render() {
+    const { ownerId, haveEatingPlace } = this.props;
+    console.log(haveEatingPlace);
+    let owner;
+    if (haveEatingPlace) {
+      owner = haveEatingPlace.owner;
     }
+    return (
+      <div>
+        <PostItem />
+        {ownerId === owner ? <PostForm /> : null}
+      </div>
+    );
+  }
 }
-export default Posts;
+
+const mapStateToProps = state => {
+  return {
+    ownerId: state.auth.userId,
+    haveEatingPlace: state.auth.haveEatingPlace
+  };
+};
+export default connect(mapStateToProps)(Posts);
