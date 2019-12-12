251d47241f9f176e0b1af2bc9f21d9cafacc34f6
Artur Rowinski <arowinski96@gmail.com>
2019-12-12 15:50:44 +0100

    Add getting query parameters from url

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -13,7 +13,8 @@ import SecondForm from "../../components/OwnerEatingPlace/AddEatingPlace/SecondF
 import SummaryForm from "../../components/OwnerEatingPlace/AddEatingPlace/SummaryForm/SummaryForm";
 import { connect } from "react-redux";
 import LogOut from "../../components/Auth/LogOut/LogOut";
-import ForgotPasswordView from "../ForgotPasswordView/ForgotPasswordView";
+import ForgotPasswordView from "../ForgotPasswordView/ForgotPasswordView"
+import UserActionView from "../UserActionView/UserActionView";
 import * as actions from "../../store/actions/index";
 import PrivateRoute from "../../components/Common/PrivateRoute";
 import E404 from "../Errors/HTTP/404";
@@ -71,7 +72,8 @@ class Root extends React.Component {
             <Route path="/logout" component={LogOut} />
             <Route path="/register" component={RegisterView} />
             <Route path="/register-success" component={RegisterSuccessView} />
-            <Route path="/forgot-password" component={ForgotPasswordView} />
+            <Route path="/user-action" component={UserActionView} />
+            <Route path="/forgot-password" component={ForgotPasswordView}
             <PrivateRoute path="/owner-home" component={OwnerContent} />
             <PrivateRoute
               path="/add-eating-place-first-form"
