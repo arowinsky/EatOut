d6e35f91f4446adff743b2b5b41d288774ad000e
Artur Rowinski <arowinski96@gmail.com>
2020-01-22 10:01:34 +0100

    Changes: * Add form to edit user data * Add showing currently user data in inputs

--- src/components/AccountSettings/AccountSettings.js
+++ src/components/AccountSettings/AccountSettings.js
@@ -3,22 +3,38 @@ import styles from "./AccountSettings.module.scss";
 import Button from "../Button/Button";
 import { connect } from "react-redux";
 import * as actions from "../../store/actions/index";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 class AccountSettings extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      sendedRequest: null
+      sendedRequest: null,
+      userWantEditData: null
     };
   }
+
+  editUserData = () => {
+    this.setState(() => ({ userWantEditData: true }));
+  };
   render() {
-    const { sendedRequest } = this.state;
+    const { sendedRequest, userWantEditData } = this.state;
     const { accountData } = this.props;
     console.log(accountData);
+    let lastName;
+    let firstName;
+    let username;
+    let email;
     if (sendedRequest === null) {
       this.setState({ sendedRequest: true });
       let z = localStorage.getItem("z");
       console.log(z);
       this.props.getUserData(z);
+    }
+    if (accountData) {
+      firstName = accountData.firstName;
+      lastName = accountData.lastName;
+      username = accountData.username;
+      email = accountData.email;
     }
     return (
       <div className={styles.wrapper}>
@@ -26,21 +42,68 @@ class AccountSettings extends React.Component {
 
         <div className={styles.content}>
           <div className={styles.title}>Podstawowe dane</div>
-          {accountData ? (
+          {userWantEditData ? (
+            <Formik
+              initialValues={{
+                firstName: firstName,
+                lastName: lastName,
+                username: username,
+                email: email
+              }}
+            >
+              {() => (
+                <Form className={styles.userEditDataForm}>
+                  <div className={styles.inputElement}>
+                    <label htmlFor="firstName">Imię</label>
+                    <Field
+                      type="text"
+                      name="firstName"
+                      className={styles.input}
+                    />
+                  </div>
+                  <br />
+                  <div className={styles.inputElement}>
+                    <label htmlFor="lastName">Nazwisko</label>
+                    <Field
+                      type="text"
+                      name="lastName"
+                      className={styles.input}
+                    />
+                  </div>
+                  <br />
+                  <div className={styles.inputElement}>
+                    <label htmlFor="username">Nazwa użytkownika</label>
+                    <Field
+                      type="text"
+                      name="username"
+                      className={styles.input}
+                    />
+                  </div>
+                  <br />
+                  <div className={styles.inputElement}>
+                    <label htmlFor="email">Email</label>
+                    <Field type="text" name="email" className={styles.input} />
+                  </div>
+                  <br />
+                  <Button second type="submit" className={styles.button}>
+                    Zapisz zmiany
+                  </Button>
+                </Form>
+              )}
+            </Formik>
+          ) : (
             <div className={styles.basicData}>
-              <div className={styles.info}>Imię: {accountData.firstName}</div>
-              <div className={styles.info}>
-                Nazwisko: {accountData.lastName}
+              <div className={styles.info}>Imię: {firstName}</div>
+              <div className={styles.info}>Nazwisko: {lastName}</div>
+              <div className={styles.info}>Nazwa użytkownika: {username}</div>
+              <div className={styles.info}>Email: {email}</div>
+              <div className={styles.buttonBasicData}>
+                <Button second onClick={this.editUserData}>
+                  Edytuj dane
+                </Button>
               </div>
-              <div className={styles.info}>
-                Nazwa użytkownika: {accountData.username}
-              </div>
-              <div className={styles.info}>Email: {accountData.email}</div>
             </div>
-          ) : null}
-          <div className={styles.buttonBasicData}>
-            <Button second>Edytuj dane</Button>
-          </div>
+          )}
         </div>
         <div className={styles.content}>
           <div className={styles.title}>Konto właściciela</div>
