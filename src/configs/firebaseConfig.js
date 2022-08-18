1da1175cb188fcc86fae6ad6a979e08f5c56ff3a
Artur Rowiński <arowinski96@gmail.com>
2022-08-18 18:55:53 +0200

    Security update firebaseConfig.js

--- src/configs/firebaseConfig.js
+++ src/configs/firebaseConfig.js
@@ -4,17 +4,15 @@ import "firebase/auth";
 import "firebase/storage";
 
 export const config = {
-  apiKey: "AIzaSyAaJRfgtMU3LqvV07NyiaGfqUj_XGpkoNo",
-  authDomain: "eatout-faae0.firebaseapp.com",
-  databaseURL: "https://eatout-faae0.firebaseio.com",
-  projectId: "eatout-faae0",
-  storageBucket: "eatout-faae0.appspot.com",
-  messagingSenderId: "734346628660"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
 };
 
 firebase.initializeApp(config);
 let storage = firebase.storage();
-
-// let db = firebase.firestore();
 
 export default storage;
